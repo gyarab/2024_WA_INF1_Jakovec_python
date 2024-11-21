c076213e40751becdbd4a908a100e58ad069ac15
Viktor Jakovec <viktor.jakovec@student.gyarab.cz>
2024-11-21 11:38:16 +0100

    gfujmhsazedgfsdfjhggfdgfdgfdskuzgfds

--- test.py
+++ test.py
@@ -1,10 +1,17 @@
-def is_prime(number):
-    if(isinstance(number, int) == False):
-        return False
-    elif (number <= 0):
-    if number <= 1:
-        return False
-    for i in range(2, int(number ** 0.5) + 1):
-        if number % i == 0:
-            return False
-    return True
+def primes_in_range(a, b):
+    if not isinstance(a, int) or not isinstance(b, int):
+        raise ValueError("...invalid input...")
+    
+    if a > b:
+        a, b = b, a
+    
+    primes = []
+    for num in range(a, b + 1):
+        if num > 1:
+            for i in range(2, int(num/2) + 1):
+                if (num % i) == 0:
+                    break
+            else:
+                primes.append(num)
+    
+    return primes
