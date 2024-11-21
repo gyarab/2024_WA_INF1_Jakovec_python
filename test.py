5ce38cb9030208b8a83a39f46356821fc627f4d4
Viktor Jakovec <viktor.jakovec@student.gyarab.cz>
2024-11-21 11:01:15 +0100

    haha

--- test.py
+++ test.py
@@ -5,3 +5,29 @@ def celsius_to_fahrenheit(celsius):
 def fahrenheit_to_celsius(fahrenheit):
     celsius = (fahrenheit - 32) * 5/9
     return celsius
+
+def fibonacci():
+    while True:
+        try:
+            user_input = int(input("Enter an integer: "))
+            if user_input <= 0:
+                print("Please enter a positive integer.")
+                continue
+            break
+        except ValueError:
+            print("Invalid input. Please enter an integer.")
+
+    if user_input == 1:
+        return [0]
+    elif user_input == 2:
+        return [0, 1]
+    else:
+        fib_sequence = [0, 1]
+        while len(fib_sequence) < user_input:
+            next_num = fib_sequence[-1] + fib_sequence[-2]
+            fib_sequence.append(next_num)
+        return fib_sequence
+
+
+fib_sequence = fibonacci()
+print("Fibonacci sequence:", fib_sequence)
