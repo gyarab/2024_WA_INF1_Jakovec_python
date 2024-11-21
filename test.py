a5d5c9ef2502a704ec4c8faba7aafd84d2277872
Viktor Jakovec <viktor.jakovec@student.gyarab.cz>
2024-11-21 10:58:22 +0100

    ctf ftc

--- test.py
+++ test.py
@@ -1,25 +1,7 @@
-def fibonacci(user_input):
-    while True:
-        try:
-            if user_input <= 0:
-                continue
-            break
-        except ValueError:
-            print("Invalid input. Please enter an integer.")
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
 
-    # Generate Fibonacci sequence
-    if user_input == 1:
-        return [0]
-    elif user_input == 2:
-        return [0, 1]
-    else:
-        fib_sequence = [0, 1]
-        while len(fib_sequence) < user_input:
-            next_num = fib_sequence[-1] + fib_sequence[-2]
-            fib_sequence.append(next_num)
-        return fib_sequence
-
-
-# Call the fibonacci() function and print the result
-fib_sequence = fibonacci()
-print("Fibonacci sequence:", fib_sequence)
+def fahrenheit_to_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) * 5/9
+    return celsius
