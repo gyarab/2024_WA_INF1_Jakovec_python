366b0543c77e87f0c79cad0a1c25da1518f1923c
Viktor Jakovec <viktor.jakovec@student.gyarab.cz>
2024-11-28 10:49:24 +0100

    fix

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 def rotate_array(arr, n):
-    if isinstance(arr, list) and all(isinstance(x, int) for x in arr):
+    if isinstance(arr, list) and all(isinstance(x, int) for x in arr) and isinstance(n, int):
         return arr[-n:] + arr[:-n]
     else:
         return "Invalid input"
+print (rotate_array([1, 2, 3, 4, 5, 6], "d")) # [5, 6, 1, 2, 3, 4]
