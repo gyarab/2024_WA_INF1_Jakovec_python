e49baaf33e1f6f794cd2d844408e4e7936bb4fc5
ViktorJakove <117266689+ViktorJakove@users.noreply.github.com>
2025-02-27 11:04:53 +0100

    Delete databaze_punku directory

--- databaze_punku/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('band/<int:band_id>/', views.band_detail, name='band_detail'),
-    path('album/<int:album_id>/', views.album_detail, name='album_detail'),
-    path('search/', views.search, name='search'),
-]
